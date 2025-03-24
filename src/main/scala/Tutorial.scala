b766f1802b0f0418ded6042541a665e517904414
Valentin Hassinen <darkfrobozz.mega@gmail.com>
2025-03-24 17:23:46 +0100

    added some more thereoms

--- src/main/scala/Tutorial.scala
+++ src/main/scala/Tutorial.scala
@@ -4,8 +4,30 @@ import stainless.lang.*
 object Tutorial {
 
   // You can start writing your code here
-  def fac(n: Int) : Int = {
-    if (n == 0) 1 else fac(n-1) * n
-  }
+  // def fac(n: BigInt) : BigInt = {
+  //   require(n >= 0)
+  //   // decreases(n)
+  //   if (n == 0) 1 else fac(n-1) * n
+  // }
 
+
+  // def dividesSum(c: BigInt, x: BigInt, y: BigInt, a: BigInt, b : BigInt) = {
+  //   require(c > 0 && a % c == 0 && b % c == 0)
+  // }.ensuring((x * a + y * b) % c == 0)
+
+  /**
+    * 
+    *
+    * @param c This is a divisor of a and b
+    * @param a 
+    * @param b
+    */
+  // def conserveGcd(c: BigInt, a: BigInt, b : BigInt) = {
+  //   require(a % c == 0 && b % c == 0)
+  // }.ensuring((a % b) % c == 0)
+
+  def gcd(a: BigInt, b: BigInt) : BigInt = {
+    require(a >= b && b > 0)
+    if (a % b == 0) b else gcd(b, a % b)
+  }.ensuring(res => res >= 1)
 }
--- src/test/scala/testSuite.scala
+++ src/test/scala/testSuite.scala
@@ -2,8 +2,15 @@
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("Testing Fibonnaci") {
-    val obtained = Tutorial.fac(4)
-    val expected = 24
+    val obtained = Tutorial.fac(BigInt(4))
+    val expected = BigInt(24)
+    assertEquals(obtained, expected)
+  }
+
+
+  test("Testing gcd") {
+    val obtained = Tutorial.gcd(BigInt(4), BigInt(26))
+    val expected = BigInt(2)
     assertEquals(obtained, expected)
   }
 }
